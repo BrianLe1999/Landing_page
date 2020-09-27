277e10dde9492271c4e45e1ac3421a595a39dc4b
BrianLe1999 <minhnguyenlesacramento@gmail.com>
2020-09-27 14:41:18 -0700

    Updates

---
+++ precache-manifest.b919cfc81d83cc5812a5226df98b3cf5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179cb9ab2f473ba5dc8e33bb6f1ba56d",
+    "url": "/Tomo-Coding-Assignment/index.html"
+  },
+  {
+    "revision": "119d37129eadea7f01ff",
+    "url": "/Tomo-Coding-Assignment/static/css/main.8388ed4d.chunk.css"
+  },
+  {
+    "revision": "c27971a1f11edb83b24b",
+    "url": "/Tomo-Coding-Assignment/static/js/2.67c91315.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tomo-Coding-Assignment/static/js/2.67c91315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119d37129eadea7f01ff",
+    "url": "/Tomo-Coding-Assignment/static/js/main.8c72678b.chunk.js"
+  },
+  {
+    "revision": "91f2c3950ea466f2ea0b",
+    "url": "/Tomo-Coding-Assignment/static/js/runtime-main.692c8b3d.js"
+  },
+  {
+    "revision": "37b5a1e70d28e0251d4dbe331de3db29",
+    "url": "/Tomo-Coding-Assignment/static/media/bootstrap.37b5a1e7.jpg"
+  },
+  {
+    "revision": "91db0751f17876aac87e28a2033e3e76",
+    "url": "/Tomo-Coding-Assignment/static/media/easy-to-use.91db0751.jpg"
+  },
+  {
+    "revision": "dbfff7f304afb1dc5bbd81a3ddba1cb0",
+    "url": "/Tomo-Coding-Assignment/static/media/responsive.dbfff7f3.jpg"
+  },
+  {
+    "revision": "20cbd480edd75dd476f3d16c6a763f28",
+    "url": "/Tomo-Coding-Assignment/static/media/sign-up-background.20cbd480.jpg"
+  },
+  {
+    "revision": "828e0935e45611b23593d139bd1984f1",
+    "url": "/Tomo-Coding-Assignment/static/media/testimony-1.828e0935.jpg"
+  },
+  {
+    "revision": "f3c0271e2279f2bdeea02ddc3aa03dc5",
+    "url": "/Tomo-Coding-Assignment/static/media/testimony-2.f3c0271e.jpg"
+  },
+  {
+    "revision": "15393c5a215a67b0f4bbc4d4ed55f12c",
+    "url": "/Tomo-Coding-Assignment/static/media/testimony-3.15393c5a.jpg"
+  }
+]);
